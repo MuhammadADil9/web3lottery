cc1fdb7aabd38bb4265f7e224e6593af3e0e6b88
Adil <madilaslam9@gmail.com>
2024-09-20 23:53:02 +0500

    friday

--- src/lottery.sol
+++ src/lottery.sol
@@ -1,21 +1,17 @@
 //SPDX-License-Identifier : MIT;
 
+/* Errors */
+error lottery_timeNotPassed();
+error lottery_insufficientBalance();
 
-// Instructions
 
-//lottery contract
-//people will enter into the lottery by inserting certian amount into it.
-//after certian time someone will be declared as winner automatically 
 
-//enternace function
-//selecting winner function
 
 /**
  * @author Adil
  * @notice Lottery project allowing user to enter with certian amount and giving them chance to win 
  * @dev Implementation of VRF and Keepers for randomnees and automation respectively.
  */
-
 
 /*Contract*/
 
@@ -24,34 +20,51 @@ contract lottery{
     /*State varibales*/
     uint256 private immutable i_enteranceFee;
     address private immutable i_owner;
-    struct public participant {
+    uint256 private immutable i_timeLimit;
+    uint256 private immutable i_lastTimeContractDeployed;
+
+    struct participant {
         uint256 amount;
         address payable userAddress;
     }
-    participant[] public payable participants;
-    
+
+    participant[]  public participants;
+
     
     /*Functions*/
     constructor(){
         i_owner = msg.sender;
+        i_enteranceFee = 1 ether;
+        i_timeLimit = 4000;
+        i_lastTimeContractDeployed = block.timestamp;
     }
 
     function enterRafle() payable external {
-        participants.push(publicparticipant({
+        
+        if(msg.value < i_enteranceFee){
+            revert lottery_insufficientBalance();
+        }
+        
+        participants.push(participant({
             amount : msg.value,
-            userAddress : msg.sender
+            userAddress : payable(msg.sender)
         }));
     }
 
+
     function selectWinner() public {
-
+        if(block.timestamp-i_lastTimeContractDeployed < i_timeLimit){
+            revert lottery_timeNotPassed();
+        }
     }
-
-    public getUser(uint256 counter) public view return( address user , uint256 amount){
-        (user,amount) = participants[counter];
+    
+    /* Getters Functions */
+    function getUser(uint256 counter) public view returns( address user , uint256 amount){
+        participant memory temp = participants[counter];
+        amount = temp.amount;
+        user = temp.userAddress;
          return (user,amount);
     }
-
 
 
 
