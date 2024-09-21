f01ae602ed9364346c592e740df2148cb10c1b43
Adil <madilaslam9@gmail.com>
2024-09-22 02:23:36 +0500

    adding VRF functionality

--- src/lottery.sol
+++ src/lottery.sol
@@ -1,5 +1,10 @@
 //SPDX-License-Identifier : MIT;
 
+/*Imports*/
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/dev/vrf/libraries/VRFV2PlusClient.sol";
+
 /* Errors */
 error lottery_timeNotPassed();
 error lottery_insufficientBalance();
@@ -15,13 +20,22 @@ error lottery_insufficientBalance();
 
 /*Contract*/
 
-contract lottery{
+contract lottery is VRFConsumerBaseV2Plus{
 
     /*State varibales*/
     uint256 private immutable i_enteranceFee;
     address private immutable i_owner;
     uint256 private immutable i_timeLimit;
     uint256 private immutable i_lastTimeContractDeployed;
+
+    uint256 requestId;
+    bytes32 keyHash;
+    uint256 subId;
+    uint16 requestConfirmations;
+    uint32 callbackGasLimit;
+    uint32 numWords;
+    bytes extraArgs;
+
 
     struct participant {
         uint256 amount;
@@ -32,7 +46,7 @@ contract lottery{
 
     
     /*Functions*/
-    constructor(){
+    constructor(address vrfCordinator) VRFConsumerBaseV2Plus(vrfCordinator) {
         i_owner = msg.sender;
         i_enteranceFee = 1 ether;
         i_timeLimit = 4000;
@@ -56,8 +70,26 @@ contract lottery{
         if(block.timestamp-i_lastTimeContractDeployed < i_timeLimit){
             revert lottery_timeNotPassed();
         }
+
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: subId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                   VRFV2PlusClient.ExtraArgsV1({
+                       nativePayment: enableNativePayment
+                   })
+                )
+            }));
     }
-    
+
+
+
+
+
     /* Getters Functions */
     function getUser(uint256 counter) public view returns( address user , uint256 amount){
         participant memory temp = participants[counter];
