e747d4012a8134b0348df89650a74d7b4bbdadd5
Adil <madilaslam9@gmail.com>
2024-09-23 00:29:21 +0500

    struct error

--- src/lottery.sol
+++ src/lottery.sol
@@ -1,7 +1,6 @@
-//SPDX-License-Identifier : MIT;
+// SPDX-License-Identifier: MIT
 
-/*Imports*/
-
+/* Imports */
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/dev/vrf/libraries/VRFV2PlusClient.sol";
 
@@ -9,95 +8,86 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/dev/vrf/libraries/V
 error lottery_timeNotPassed();
 error lottery_insufficientBalance();
 
-
-
-
 /**
  * @author Adil
- * @notice Lottery project allowing user to enter with certian amount and giving them chance to win 
- * @dev Implementation of VRF and Keepers for randomnees and automation respectively.
+ * @notice Lottery project allowing users to enter with a certain amount and giving them a chance to win
+ * @dev Implementation of VRF and Keepers for randomness and automation respectively.
  */
 
-/*Contract*/
+/* Contract */
 
-contract lottery is VRFConsumerBaseV2Plus{
-
-    /*State varibales*/
+contract lottery is VRFConsumerBaseV2Plus {
+    /* State variables */
     uint256 private immutable i_enteranceFee;
     address private immutable i_owner;
     uint256 private immutable i_timeLimit;
     uint256 private immutable i_lastTimeContractDeployed;
 
-    uint256 requestId;
-    bytes32 keyHash;
-    uint256 subId;
-    uint16 requestConfirmations;
-    uint32 callbackGasLimit;
-    uint32 numWords;
-    bytes extraArgs;
-
+    /* Request Parameters */
+    uint32 private immutable i_numWords;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint256 private subId;
 
     struct participant {
         uint256 amount;
         address payable userAddress;
     }
 
-    participant[]  public participants;
+    participant[] public participants;
 
-    
-    /*Functions*/
-    constructor(address vrfCordinator) VRFConsumerBaseV2Plus(vrfCordinator) {
+    /* Constructor */
+    constructor(address vrfCordinator, uint32 _callBackGas, bytes32 _keyhash, uint256 _subId)
+        VRFConsumerBaseV2Plus(vrfCordinator)
+    {
         i_owner = msg.sender;
         i_enteranceFee = 1 ether;
         i_timeLimit = 4000;
         i_lastTimeContractDeployed = block.timestamp;
+        i_numWords = 1;
+        i_callbackGasLimit = _callBackGas;
+        i_keyHash = _keyhash;
+        subId = _subId;
     }
 
-    function enterRafle() payable external {
-        
-        if(msg.value < i_enteranceFee){
+    /* Function to enter the lottery */
+    function enterRafle() external payable {
+        if (msg.value < i_enteranceFee) {
             revert lottery_insufficientBalance();
         }
-        
-        participants.push(participant({
-            amount : msg.value,
-            userAddress : payable(msg.sender)
-        }));
+        participants.push(participant({amount: msg.value, userAddress: payable(msg.sender)}));
     }
 
-
-    function selectWinner() public {
-        if(block.timestamp-i_lastTimeContractDeployed < i_timeLimit){
+    /* Function to select the winner */
+    function selectWinner() external {
+        if (block.timestamp - i_lastTimeContractDeployed < i_timeLimit) {
             revert lottery_timeNotPassed();
         }
 
-        requestId = s_vrfCoordinator.requestRandomWords(
-            VRFV2PlusClient.RandomWordsRequest({
-                keyHash: keyHash,
-                subId: subId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                   VRFV2PlusClient.ExtraArgsV1({
-                       nativePayment: enableNativePayment
-                   })
-                )
-            }));
+        // Use VRFV2PlusClient.RandomWordsRequest directly
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: subId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: i_numWords,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+
+        // uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
+    /* Function to handle the fulfillment of random words */
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
 
-
-
-
-    /* Getters Functions */
-    function getUser(uint256 counter) public view returns( address user , uint256 amount){
+    /* Getter function to retrieve participant information */
+    function getUser(uint256 counter) public view returns (address user, uint256 amount) {
         participant memory temp = participants[counter];
         amount = temp.amount;
         user = temp.userAddress;
-         return (user,amount);
+        return (user, amount);
     }
-
-
-
 }
