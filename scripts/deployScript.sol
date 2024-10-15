2d747fb7335686a1995135b5cd96d5d429a246e7
Adil <madilaslam9@gmail.com>
2024-10-15 14:35:08 +0500

    prior to solving the vm issue

--- scripts/deployScript.sol
+++ scripts/deployScript.sol
@@ -2,8 +2,9 @@
 pragma solidity^0.8.9;
 
 import {lottery} from "../src/lottery.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
 
-contract deployContract{
+contract deployContract is Script{
 
     function deployContractFunction() internal view returns(lottery){
         lottery LotteryContract = new lottery();
