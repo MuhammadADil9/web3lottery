6f7caf032841d2551850b38237e0344674d06e85
Adil <madilaslam9@gmail.com>
2024-11-28 03:21:12 +0500

    fulfil random words done

--- src/rafle.sol
+++ src/rafle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 //net spac for making the contract more beautiful
 /**
  * @title Lottery contract
@@ -9,25 +12,30 @@ pragma solidity 0.8.19;
  * @notice This is a lottery smart contract. Join us and receive a chance of winning handsome amount of mon
  */
 
-//pragma > versioning > imports > comments
 
-// functions for entering into the contract
-// functions for winning the lottery
+contract raffle is VRFConsumerBaseV2Plus {
 
-// errors
-// error if the participant fails to submit sufficient amount of money in the contract
-
-contract raffle {
     /** State Variables */
 
     uint256 private immutable i_EnteranceFee;
     address private immutable i_Owner;
+
+    // variables required for VRF
+    uint256 s_subscriptionId;
+    address vrfCoordinator;
+    bytes32 s_keyHash;
+    uint32 private constant CALLBACKGASLIMIT = 5000;
+    uint16 private constant REQUESTCONFIRMATION = 2;
+    uint32 private constant NUMOFWORDS = 1;
+
+
     //struct for storing the user information
     struct user {
         address payable participant;
         string name;
         uint256 time;
     }
+
     // array of structure;
     user[] public s_users;
     //time variable
@@ -37,11 +45,15 @@ contract raffle {
     
     
     /** Functions */
-    constructor(uint256 fee, uint256 time) {
+    constructor(uint256 fee, uint256 time, uint256 subId, bytes32 keyHash, address vrfCordinatorAddress ) VRFConsumerBaseV2Plus(vrfCordinatorAddress)   {
         i_EnteranceFee = fee;
         i_Owner = msg.sender;
         i_timeLimit = time;
         LotterylastTime = block.timestamp;
+        s_subscriptionId = subId;
+        s_keyHash = keyHash;
+        vrfCoordinator = vrfCordinatorAddress;
+
     }
 
     function enterRafle(string calldata name) public payable {
@@ -58,16 +70,64 @@ contract raffle {
     }
 
     function winningRafle() public {
-        //logic
+        
+        // checking if not enough time has passed
+        
+        if(block.timestamp - LotterylastTime < i_timeLimit){
+            revert rafle_notEnoughTimePassed();
+        }
+
+        // reverting with a error if there are less than 5 people within the array 
+
+        if(s_users.length <5){
+            revert rafle_notEnoughPersonInLottery();
+        }
+
+        // requesting random word
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: REQUESTCONFIRMATION,
+                callbackGasLimit: CALLBACKGASLIMIT,
+                numWords: NUMOFWORDS,
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: true}))
+            })
+        );
+        
     }
 
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // No checks 
+        // Effects 
+        uint256 number = randomWords[0];
+        number = s_users.length % number;
+        // user memory temp = 
+        address winner = s_users[number].participant;
+        uint256 balance = address(this).balance;
+        uint256 winnerAmount = balance - 2.5 ether;
+        //transfering the amount the winner
+        (bool sucess,) = winner.call{value:winnerAmount}("");
+        if(!sucess){
+            revert rafle_amountNotSentToWinner();
+        }
+        emit amountPaidToWinner(winner,s_users[number].name);
+        LotterylastTime = block.timestamp;
+        delete s_users;
+    }
     
     
     /** Events */
     event updateParticipants(string indexed name, address indexed userAddress);
-
+    event amountPaidToWinner(address indexed winnerAddress, string indexed winnerName);
     
     /** Errors */
     error rafle_insufficientBalance(string reason, uint256 requiredAmount);
     error rafle_notEnoughTimePassed();
+    error rafle_notEnoughPersonInLottery();
+    error rafle_amountNotSentToWinner();
 }
+
+
+
