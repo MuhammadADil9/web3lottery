99630dd7fd5c15c704d359475b610f3c8cc100cd
MuhammadADil9 <madilaslam9@gmail.com>
2025-02-03 15:39:50 +0500

    installed chianlink libraries

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -3,11 +3,36 @@ pragma solidity ^0.8.20;
 import {NetworkConfiguration} from "./networkConfiguration.s.sol";
 import {Script,console} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
-// import {}
+import {constantWords} from "networkConfiguration.s.sol";
 
-contract subscriptionContract is Script { 
+contract subscriptionContract is Script,constantWords { 
     function run() public {
-        createSubscriptionUsingConfig();
+        // createSubscriptionUsingConfig();
+    }
+
+    function createSepoliaSubscription(address vrf) public returns(uint256){
+        //VRFCordinator will be of sepolia 
+        uint256 id = irvfCordinator(vrf).createSubscription();
+        return id;        
+    } 
+    
+    function SepoliaSubscription(address vrf) public returns (uint256){
+        return createSepoliaSubscription();
+    } 
+
+
+    function createAnvilSubscription(address vrf) public returns(uint256){
+        //VRFCordinator will be of local anvil chian that is mock 
+        uint256 id = VRFCoordinatorV2_5Mock(vrf).createSubscription();
+        return id;        
+    } 
+    
+    function AnvilSubscription(address vrf) public returns (uint256){
+        return createSepoliaSubscription();
+    } 
+    
+    function createSubscription(address vrf) public returns(uint256){
+        if(block.chainid==)
     }
 
     function createSubscriptionUsingConfig() public returns(uint256 id,address vrfCordinatorAddres){ 
